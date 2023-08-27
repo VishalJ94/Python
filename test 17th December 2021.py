3d34eb4e1990faa3ea4bdc225906d9b4936e55b2
VishalJ94 <62827108+VishalJ94@users.noreply.github.com>
2023-08-27 20:20:55 +0530

    Add files via upload

---
+++ test 17th December 2021.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 17 19:11:31 2021
+
+@author: 
+"""
+# def input_numbers():
+#     a=int(input("Kindly provide first number:"))
+#     b=int(input("Kindly provide second number:"))
+#     n=0
+#     while (n<b):
+#         print(a)
+#         a=a+1
+#         n=n+1
+        
+# input_numbers()
+
+
+a=int(input("Kindly provide first number:"))
+b=int(input("Kindly provide second number:"))
+n=[i for i in range(0,(a+(b*2)+1)) if i%2==0]
+
+# def evennumber():
+#     for i in n:
+#         if i>=a:
+#             return i        
+    
+# def printtype():
+#     for i in range(0,b):
+#         print(evennumber())
+
+def nexteven():
+    for i in n:
+        if i>=a:
+            print(i)
+            
+        
+        
+    
